d3e8f134178d92f2195e322bd7918f0842791d44
Satya Pavan <pavan.simham@gmail.com>
2018-01-25 21:03:11 +0530

    added the onload method, yet to implement it

---
+++ js/script.js
@@ -0,0 +1,6 @@
+function drawGameBoard() {
+	console.log("Entered into drawGameBoard");
+	// TODO - this is a place holder for creating the table/game board on the fly at a later point of time.
+}
+
+window.addEventListener("DOMContentLoaded", drawGameBoard, false);
