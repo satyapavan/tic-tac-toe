af0ca721228984375106b6d6b573f57e658ae0c3
Satya Pavan <pavan.simham@gmail.com>
2018-02-10 00:53:35 +0530

    completed post game banner

--- js/ui.js
+++ js/ui.js
@@ -6,18 +6,18 @@
 const svg_x = '<svg class="crosses" aria-label="X" role="img" viewBox="0 0 128 128" ><path d="M16,16L112,112" style="stroke: rgb(84, 84, 84); "></path><path d="M112,16L16,112" style="stroke: rgb(84, 84, 84); "></path></svg>';
 const svg_o = '<svg class="noughts" aria-label="O" role="img" viewBox="0 0 128 128" ><path d="M64,16A48,48 0 1,0 64,112A48,48 0 1,0 64,16" style="stroke: rgb(242, 235, 211);"></path></svg>';
 
-const svg_o_won = '<svg class="noughts" aria-label="O" role="img" viewBox="0 0 128 128" style="width: 128px; height: 128px;"><path d="M64,16A48,48 0 1,0 64,112A48,48 0 1,0 64,16" style="stroke: rgb(242, 235, 211);"></path></svg>';
-
-const svg_x_won = '<svg class="crosses" aria-label="X" role="img" viewBox="0 0 128 128" style="width: 128px; height: 128px;"><path d="M16,16L112,112" style="stroke: rgb(84, 84, 84);"></path><path d="M112,16L16,112" style="stroke: rgb(84, 84, 84);"></path></svg>';
-
-const svg_tie = '<svg class="crosses" aria-label="X" role="img" viewBox="0 0 128 128" style="width: 96px; height: 96px;"><path d="M16,16L112,112" style="stroke: rgb(84, 84, 84);"></path><path d="M112,16L16,112" style="stroke: rgb(84, 84, 84);"></path></svg>\
+const svg_xo = '<svg class="crosses" aria-label="X" role="img" viewBox="0 0 128 128" style="width: 96px; height: 96px;"><path d="M16,16L112,112" style="stroke: rgb(84, 84, 84);"></path><path d="M112,16L16,112" style="stroke: rgb(84, 84, 84);"></path></svg>\
 	<svg class="noughts" aria-label="O" role="img" viewBox="0 0 128 128" style="width: 96px; height: 96px;"><path d="M64,16A48,48 0 1,0 64,112A48,48 0 1,0 64,16" style="stroke: rgb(242, 235, 211);"></path></svg>';
 
 var UI = {};
 
 UI.drawBoard = function() {
 	console.log("Entering into drawBoard");
-	var gameboard_code = '<table class="table text-center">\
+	var gameboard_code = '\
+			<div id="x-won" class="container">' + svg_x + '<span>WINNER</span></div>\
+			<div id="o-won" class="container">' + svg_o + '<span>WINNER</span></div>\
+			<div id="tie"   class="container">' + svg_xo + '<span>It\'s a TIE</span></div>\
+			<table class="table text-center">\
 				<tr class="c_row_1">\
 					<td id="cell-11" class="c_col_1" role="button" tabindex="0" data-row="0" data-col="0"></td>\
 					<td id="cell-12" class="c_col_2" role="button" tabindex="0" data-row="0" data-col="1"></td>\
@@ -70,26 +70,39 @@ UI.updateScreen = function(parGameState) {
 
 		///////////////////////////////////////////////////////////
 		case parGameState.RESULTS.playerXWon :
-			document.getElementById("messageboard").innerHTML = "<br> '" + parGameState.SYMBOL.human + "' WON!";
-			// document.getElementById("game_table").innerHTML = svg_x_won;
+			document.getElementById("messageboard").innerHTML = "<br> Gameover";
+			document.getElementById("score-o").classList.add('focus-score-o');
+			document.getElementById("score-x").classList.add('focus-score-x');
 
+			var div = document.getElementById("x-won");
+			div.style.display = "block"; 
 		break;
 
 		///////////////////////////////////////////////////////////
 		case parGameState.RESULTS.playerOWon :
-			document.getElementById("messageboard").innerHTML = "<br> '" + parGameState.SYMBOL.robot + "' WON!";
-			// document.getElementById("game_table").innerHTML = svg_o_won;
+			document.getElementById("messageboard").innerHTML = "<br> Gameover";
+			document.getElementById("score-o").classList.add('focus-score-o');
+			document.getElementById("score-x").classList.add('focus-score-x');
+			
+			var div = document.getElementById("o-won");
+			div.style.display = "block"; 
 		break;
 
 		///////////////////////////////////////////////////////////
 		case parGameState.RESULTS.tie :
-			document.getElementById("messageboard").innerHTML = "<br>It's a TIE!!";
-			// document.getElementById("game_table").innerHTML = svg_tie;
+			document.getElementById("messageboard").innerHTML = "<br> Gameover";
+			document.getElementById("score-o").classList.add('focus-score-o');
+			document.getElementById("score-x").classList.add('focus-score-x');
+
+			var div = document.getElementById("tie");
+			div.style.display = "block";
 		break;
 
 		///////////////////////////////////////////////////////////
 		default:
 			console.log("What the hell happened?");
 			document.getElementById("messageboard").innerHTML = "<br>what the hell just happened? huh?";
+			document.getElementById("score-o").classList.add('focus-score-o');
+			document.getElementById("score-x").classList.add('focus-score-x');
 	}
 }
