ddbf5267aacea538553a9218f1b6fa6b87f6c6e5
Satya Pavan <pavan.simham@gmail.com>
2018-02-11 20:23:28 +0530

    moved all the gamestate related functions to that block, sort of like a class.

--- js/game_state.js
+++ js/game_state.js
@@ -30,17 +30,135 @@ var cGameState = function (level) {
 
 	// Because 'X' always gets to start the game so initialize it with it.
 	this.TURN = this.SYMBOL.human;
-};
 
-cGameState.emptyCells = function() {
-	console.log("Entering into emptyCells");
 
-	for (var row = 0; row < 3; row++) {
-		for (var col = 0; col < 3; col++) {
-			if( this.BOARD[row][col] === "" ) {
-				// available_list = 
+	this.emptyCells = function() {
+		console.log("Entering into emptyCells");
+
+		for (var row = 0; row < 3; row++) {
+			for (var col = 0; col < 3; col++) {
+				if( this.BOARD[row][col] === "" ) {
+					// available_list = 
+				}
 			}
 		}
 	}
 
-}
+
+	this.isCellMarked = function(row, col) {
+		var status = this.BOARD[row][col] !== "";
+		console.log("isCellMarked[" + status + "]");
+		return status;
+	}
+
+	this.markCell = function(row, col) {
+		if(this.isCellMarked(row, col)) {
+			console.log("Cell is already marked");
+			return false;
+		}
+
+		this.BOARD[row][col] = this.TURN ;
+
+		// BOARD[cell.getAttribute("data-row")][cell.getAttribute("data-col")] = marker;
+		this.TOTAL_MOVES++;
+
+		if(this.TURN === this.SYMBOL.robot)
+			this.O_MOVES_COUNT++ ;  // This is used for score calculation
+
+		console.log(this);
+
+		return true;
+	}
+
+	this.transitionTurn = function() {
+		console.log("Entering into transitionTurn");
+		console.log(this);
+		this.emptyCells();
+		this.TURN = this.TURN === this.SYMBOL.human ? this.SYMBOL.robot : this.SYMBOL.human;
+
+		console.log(this);
+	}
+
+	this.isGameOver = function() {
+		console.log("Entered into isGameOver");
+		
+		var line;
+		var pos;
+		var game_status = false;
+
+		// make sure there is no <= there, elese the gameover is not coming after 5 moves. yuck!
+		if(this.TOTAL_MOVES < 5){
+			console.log("Too few moves, game cannot be over yet");
+			this.GAME_STATUS = this.RESULTS.incomplete ; 
+			return false;
+		}
+
+		do {
+			// check for row success
+			for(var itr = 0; itr < 3; ++itr) {
+				line = this.BOARD[itr].join('');
+
+				if(line === this.TURN.repeat(3)) {
+					pos = [ [itr, 0], [itr, 1], [itr, 2] ];
+					game_status = true;
+					break;
+				}
+			}
+
+			// check for col success
+			for (var itr = 0; itr < 3; ++itr) {
+				line = [this.BOARD[0][itr], this.BOARD[1][itr], this.BOARD[2][itr]];
+				line = line.join('');
+
+				if(line === this.TURN.repeat(3)) {
+					pos = [ [0, itr], [1, itr], [2, itr] ];
+					game_status = true;
+					break;
+				}	
+			}
+
+			// now check for diagonal success
+
+			line = [this.BOARD[0][0], this.BOARD[1][1], this.BOARD[2][2]];
+			line = line.join('');
+
+			if(line === this.TURN.repeat(3)) {
+				pos = [ [0, 0], [1, 1], [2, 2] ];
+				game_status = true;
+				break;
+			}
+
+			// now the other diagoal check
+
+			line = [this.BOARD[0][2], this.BOARD[1][1], this.BOARD[2][0]];
+			line = line.join('');
+
+			if(line === this.TURN.repeat(3)) {
+				pos = [ [0, 2], [1, 1], [2, 0] ];
+				game_status = true;
+				break;
+			}
+
+		} while(0);
+
+		if(game_status) {
+			console.log("WINNER OF THE GAME IS " + this.TURN);
+
+			this.WINNING_LINE = pos;
+
+			if(this.TURN === this.SYMBOL.human ) {
+				this.GAME_RESULT = this.RESULTS.playerXWon ;
+			} else {
+				this.GAME_RESULT = this.RESULTS.playerOWon ;
+			}
+
+		} else if(this.TOTAL_MOVES == 9) {
+			console.log("All the steps are exhausted, its a TIE");
+			this.GAME_RESULT = this.RESULTS.tie;
+			game_status = true;
+		}
+
+		return game_status;
+	}
+};
+
--- js/script.js
+++ js/script.js
@@ -1,95 +1,5 @@
 var GAME_STATE;
 
-function isGameOver(parGameState) {
-	console.log("Entered into isGameOver");
-	
-	var line;
-	var pos;
-	var game_status = false;
-
-	// make sure there is no <= there, elese the gameover is not coming after 5 moves. yuck!
-	if(parGameState.TOTAL_MOVES < 5){
-		console.log("Too few moves, game cannot be over yet");
-		parGameState.GAME_STATUS = parGameState.RESULTS.incomplete ; 
-		return false;
-	}
-
-	do {
-		// check for row success
-		for(var itr = 0; itr < 3; ++itr) {
-			line = parGameState.BOARD[itr].join('');
-
-			if(line === parGameState.TURN.repeat(3)) {
-				pos = [ [itr, 0], [itr, 1], [itr, 2] ];
-				game_status = true;
-				break;
-			}
-		}
-
-		// check for col success
-		for (var itr = 0; itr < 3; ++itr) {
-			line = [parGameState.BOARD[0][itr], parGameState.BOARD[1][itr], parGameState.BOARD[2][itr]];
-			line = line.join('');
-
-			if(line === parGameState.TURN.repeat(3)) {
-				pos = [ [0, itr], [1, itr], [2, itr] ];
-				game_status = true;
-				break;
-			}	
-		}
-
-		// now check for diagonal success
-
-		line = [parGameState.BOARD[0][0], parGameState.BOARD[1][1], parGameState.BOARD[2][2]];
-		line = line.join('');
-
-		if(line === parGameState.TURN.repeat(3)) {
-			pos = [ [0, 0], [1, 1], [2, 2] ];
-			game_status = true;
-			break;
-		}
-
-		// now the other diagoal check
-
-		line = [parGameState.BOARD[0][2], parGameState.BOARD[1][1], parGameState.BOARD[2][0]];
-		line = line.join('');
-
-		if(line === parGameState.TURN.repeat(3)) {
-			pos = [ [0, 2], [1, 1], [2, 0] ];
-			game_status = true;
-			break;
-		}
-
-	} while(0);
-
-	if(game_status) {
-		console.log("WINNER OF THE GAME IS " + parGameState.TURN);
-
-		parGameState.WINNING_LINE = pos;
-
-		if(parGameState.TURN === parGameState.SYMBOL.human ) {
-			parGameState.GAME_RESULT = parGameState.RESULTS.playerXWon ;
-		} else {
-			parGameState.GAME_RESULT = parGameState.RESULTS.playerOWon ;
-		}
-
-	} else if(parGameState.TOTAL_MOVES == 9) {
-		console.log("All the steps are exhausted, its a TIE");
-		parGameState.GAME_RESULT = parGameState.RESULTS.tie;
-		game_status = true;
-	}
-
-	return game_status;
-}
-
-function transitionTurn(parGameState) {
-	console.log("Entering into transitionTurn");
-	console.log(parGameState);
-	parGameState.TURN = parGameState.TURN === parGameState.SYMBOL.human ? parGameState.SYMBOL.robot : parGameState.SYMBOL.human;
-
-	console.log(parGameState);
-}
-
 function computerMove() {
 	console.log("Entering into computerMove");
 	makeEaseMove();
@@ -104,19 +14,19 @@ function robotMove() {
 function playerMove() {
 	console.log("Entering into playerMove");
 
-	if( isCellMarked(this.getAttribute("data-row"), this.getAttribute("data-col"), GAME_STATE)) {
+	if( GAME_STATE.isCellMarked(this.getAttribute("data-row"), this.getAttribute("data-col"))) {
 		console.log("As the cell is already marked, do not do anything");
 		return true;
 	}
 
-	markCell(this.getAttribute("data-row"), this.getAttribute("data-col"), GAME_STATE); 
+	GAME_STATE.markCell(this.getAttribute("data-row"), this.getAttribute("data-col")); 
 
 	// first draw the X/O as per the turn
 	UI.drawSVG(this, GAME_STATE);
 	// then check if the game is over
-	if(!isGameOver(GAME_STATE)) {
+	if(!GAME_STATE.isGameOver()) {
 		// if not, then retransition to the next step/view
-		transitionTurn(GAME_STATE);
+		GAME_STATE.transitionTurn();
 	}
 	// when you feel eerything is over, then update the screen
 	UI.updateScreen(GAME_STATE);
@@ -125,30 +35,7 @@ function playerMove() {
 	robotMove();
 }
 
-function isCellMarked(row, col, parGameState) {
-	var status = parGameState.BOARD[row][col] !== "";
-	console.log("isCellMarked[" + status + "]");
-	return status;
-}
 
-function markCell(row, col, parGameState) {
-	if(isCellMarked(row, col, parGameState)) {
-		console.log("Cell is already marked");
-		return false;
-	}
-
-	parGameState.BOARD[row][col] = parGameState.TURN ;
-
-	// BOARD[cell.getAttribute("data-row")][cell.getAttribute("data-col")] = marker;
-	parGameState.TOTAL_MOVES++;
-
-	if(parGameState.TURN === parGameState.SYMBOL.robot)
-		parGameState.O_MOVES_COUNT++ ;  // This is used for score calculation
-
-	console.log(parGameState);
-
-	return true;
-}
 
 function initializeBoard() {
 	console.log("Entered into initializeBoard");
