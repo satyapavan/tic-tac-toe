aaf41eccb3701f9f7f7dbb7fb5a989c548362212
Satya Pavan <pavan.simham@gmail.com>
2018-02-16 12:18:08 +0530

    made medium a little less difficult by adding probability

--- js/ai.js
+++ js/ai.js
@@ -117,12 +117,15 @@ var AI = function() {
             return finish_moves;
         } 
 
-        // if we pass opponents move, then we get a blocking move.
-        var finish_moves = game.getFinishMoves(game.TURN === game.SYMBOL.human ? game.SYMBOL.robot : game.SYMBOL.human);
+        // without this, medium is also getting a little difficult to win and all of them are going to tie
+        if(Math.random()*100 <= probability) {
+            // if we pass opponents move, then we get a blocking move.
+            var finish_moves = game.getFinishMoves(game.TURN === game.SYMBOL.human ? game.SYMBOL.robot : game.SYMBOL.human);
 
-        if(finish_moves !== undefined && finish_moves.length > 0) {
-            return finish_moves;
-        } 
+            if(finish_moves !== undefined && finish_moves.length > 0) {
+                return finish_moves;
+            } 
+        }
 
         var available_cells = game.emptyCells();
 
