d6a67686fb368b3d583715c7ecd67f6c91fc6959
Satya Pavan <pavan.simham@gmail.com>
2018-12-22 00:22:30 +0530

    Added a little better analytics

--- js/ttt_analytics.js
+++ js/ttt_analytics.js
@@ -1,18 +1,18 @@
 document.getElementById('restartgame').addEventListener('click', function() {
 
-    gtag('config', 'UA-111600714-1', {'page_path': '/restart.html'});
+    gtag('config', 'UA-111600714-1', {'page_path': 'tic-tac-toe/restart.html'});
 
 }, false);
 
 document.getElementById('navbarSupportedContent').addEventListener('click', function() {
 
-    gtag('config', 'UA-111600714-1', {'page_path': '/difficulty_levels.html'});
+    gtag('config', 'UA-111600714-1', {'page_path': 'tic-tac-toe/difficulty_levels.html'});
 
 }, false);
 
 document.getElementById('exampleModal').addEventListener('click', function() {
 
-    gtag('config', 'UA-111600714-1', {'page_path': '/how_to_play.html'});
+    gtag('config', 'UA-111600714-1', {'page_path': 'tic-tac-toe/how_to_play.html'});
 
 }, false);
 
